bd89507b1e0caf3f54c922319dc219dcbccffea2
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-05-15 08:52:23 -0600

    Hosted App

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
 
-app.get('/', (express.static('public')))
+const PORT = process.env.PORT || 5001
 
-app.listen(5001, ()=> {
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, ()=> {
     console.log('Listening on port elasticbeanstalk')
 })
